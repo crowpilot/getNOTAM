56f1fb59baa6365cf8a0f31328c2523856b067f7
Toshiaki MATSUYAMA <toshiaki@debian>
2017-12-26 23:16:18 +0900

    mod

--- getNOTAM.sh
+++ getNOTAM.sh
@@ -3,4 +3,4 @@ nextday=`date "+%y%m%d" -d "30 days"`
 
 
 
-curl -b cookie.txt --globoff -k "https://aisjapan.mlit.go.jp/KeySearcherAction.do" -d "location=${1}" -d "period=0" -d "periodFrom=${today}0000" -d "periodTo=${nextday}0000" -d "dispScopeA=true" -d "dispScopeE=true" -d "dispScopeW=true">htmldata/${3}.html
+curl -b cookie.txt --globoff -k "https://aisjapan.mlit.go.jp/KeySearcherAction.do" -d "location=${1}" -d "period=0" -d "periodFrom=${today}0000" -d "periodTo=${nextday}0000" -d "dispScopeA=true" -d "dispScopeE=true" -d "dispScopeW=true">htmldata/${1}.html
